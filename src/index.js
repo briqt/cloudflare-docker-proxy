9d4364563ee8d757c6bf3a79d1cb3d4a551ee30c
wanggl <wanggaolei@chinahuatong.com.cn>
2024-06-20 10:55:52 +0800

    1

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.registry.briqt.dev": dockerHub,
+  "quay.registry.briqt.dev": "https://quay.io",
+  "gcr.registry.briqt.dev": "https://gcr.io",
+  "k8s-gcr.registry.briqt.dev": "https://k8s.gcr.io",
+  "k8s.registry.briqt.dev": "https://registry.k8s.io",
+  "ghcr.registry.briqt.dev": "https://ghcr.io",
+  "cloudsmith.registry.briqt.dev": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.registry.briqt.dev": dockerHub,
 };
 
 function routeByHosts(host) {
